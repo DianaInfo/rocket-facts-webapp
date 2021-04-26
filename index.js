b09ceb1bdef7f431a6d28ec24b3cdff7968f9a3d
Diana Burkart <diana.burkart@web.de>
2021-04-26 09:37:21 +0200

    add rocket motion

--- index.js
+++ index.js
@@ -7,5 +7,30 @@ window.ondevicemotion = function(event) {
 }
 
 window.addEventListener("deviceorientation", function(event) {
+	var beta = event.beta
+	var gamma = event.gamma
+
 	document.querySelector("#mag").innerHTML = "alpha = " + event.alpha + "<br>" + "beta = " + event.beta + "<br>" + "gamma = " + event.gamma;
+
+	var rocketElement = document.querySelector("#rocket_div")
+	var oldPositionTop = rocketElement.getAttribute("top")
+	var oldPositionLeft = rocketElement.getAttribute("left")
+
+	var rocketImage = document.querySelector("#rocket_image")
+	var image_path = "images/rocket_directions/rocket_fire_top_right"
+	if(beta > 0 && gamma > 0) {
+		image_path = "images/rocket_directions/rocket_fire_down_right"
+	} else if(beta > 0 && gamma < 0) {
+		image_path = "images/rocket_directions/rocket_fire_down_left"
+	} else if(beta < 0 && gamma < 0) {
+		image_path = "images/rocket_directions/rocket_fire_top_left"
+	}
+
+	rocketImage.setAttribute("src", image_path)
+
+	var newPositionTop = oldPositionTop + beta
+	var newPositionLeft = oldPositionLeft + gamma
+
+	rocketElement.setAttribute("top", newPositionTop)
+	rocketElement.setAttribute("left", newPositionLeft)
 }, true);
